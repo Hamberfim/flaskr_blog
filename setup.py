27f3028a0d245940b278378f708cf7b47555d6c0
Hamberfim <adhamlingithub@gmail.com>
2020-11-12 15:22:45 -0600

    made the project installable in an active environment

--- setup.py
+++ setup.py
@@ -8,6 +8,19 @@ This file(setup.py) and the MANIFEST.in file describe the project and the files 
 setup.py and the MANIFEST.in file are completed, Use pip to install the project in a virtual environment.
 Command to install: $ 'pip install -e .'   <-- Note the space and the period, means install in this active environment
 Once install you can see the project is now installed with: 'pip list'
+and call the app from any directory with the env active at your prompt:
+Run your Application:
+    windows - at the powershell prompt> '$env:FLASK_APP = "flaskr"'
+    windows - at the powershell prompt> '$env:FLASK_ENV = "development"'
+    windows - at the powershell prompt> 'flask run'
+    ----------------------------------------------
+    windows - at the cmd prompt> 'set FLASK_APP=flaskr'
+    windows - at the cmd prompt> 'set FLASK_ENV=development'
+    windows - at the cmd prompt> 'flask run'
+    ----------------------------------------------
+    linux/mac - at the terminal> 'export FLASK_APP=flaskr'
+    linux/mac - at the terminal> 'export FLASK_ENV=development'
+    linux/mac - at the terminal> 'flask run'
 """
 # imports
 from setuptools import find_packages
