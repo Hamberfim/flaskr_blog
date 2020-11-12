46912b39bbd0464b70c36e95a6e4b4f23b7c3779
Hamberfim <adhamlingithub@gmail.com>
2020-11-12 13:54:37 -0600

    completed application, pep8 cleanup

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -67,7 +67,7 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    # Import and register the blueprint from auth.py
+    # Import and register the auth blueprint from auth.py
     # the auth has views to register new users and to log in and log out
     from . import auth
     app.register_blueprint(auth.bp)
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -30,6 +30,8 @@ def index():
 
 
 @bp.route('/create', methods=('GET', 'POST'))
+# login_required decorator you wrote in auth.py is used on the blog views so that a user must be logged in
+# to visit these views, otherwise they will be redirected to the login page
 @login_required
 def create():
     if request.method == 'POST':
@@ -56,6 +58,13 @@ def create():
 
 
 def get_post(id, check_author=True):
+    """
+    get the post and call it from each view,
+    needed to fetch a post by id and check if the author matches the logged in user
+    :param id:
+    :param check_author:
+    :return: return post by the id
+    """
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
@@ -64,15 +73,18 @@ def get_post(id, check_author=True):
     ).fetchone()
 
     if post is None:
-        abort(404, "Post id {0} doesn't exist.".format(id))
+        abort(404, "Post id {0} doesn't exist.".format(id))  # raise a exception that returns an HTTP status code
 
+    # individual post where the user doesn't matter
     if check_author and post['author_id'] != g.user['id']:
-        abort(403)
+        abort(403)  # # raise a exception that returns an HTTP status code
 
     return post
 
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
+# login_required decorator you wrote in auth.py is used on the blog views so that a user must be logged in
+# to visit these views, otherwise they will be redirected to the login page
 @login_required
 def update(id):
     post = get_post(id)
@@ -89,11 +101,7 @@ def update(id):
             flash(error)
         else:
             db = get_db()
-            db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
-            )
+            db.execute('UPDATE post SET title = ?, body = ? WHERE id = ?', (title, body, id))
             db.commit()
             return redirect(url_for('blog.index'))
 
